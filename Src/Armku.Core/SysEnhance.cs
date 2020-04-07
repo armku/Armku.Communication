a1dd1aa21dbe11d69242e5d94a4dcb5bac00020f
Ace <258613763@qq.com>
2020-04-07 20:43:21 +0800

    增加项目

---
+++ Src/Armku.Core/SysEnhance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System
+{
+    public static class SysEnhance
+    {
+        public static int ToInt(this String str)
+        {
+            int ret = 0;
+
+            ret = Convert.ToInt32(str);
+
+            return ret;
+        }
+        public static float ToFloat(this String str)
+        {
+            float ret = 0;
+
+            ret = Convert.ToSingle(str);
+
+            return ret;
+        }
+    }
+}
+
